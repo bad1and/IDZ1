9cf1c87513d252737619e5e0303e7d2a641b8bea
bad1and <163727824+bad1and@users.noreply.github.com>
2024-10-15 23:15:34 +0300

    ver 1.0
    completed

--- main.c
+++ main.c
@@ -15,28 +15,30 @@ int bad_input(char* input, char* badChars) {
     return 0;
 }
 
-// Вывод
+// Вывод с циклом while
 void output() {
     setlocale(LC_ALL, "");
     initscr();
     printw("%s", "Bad_chars by Tikhanov Oleg\n");
-    printw("%s", "ver 0.9\n");
+    printw("%s", "ver 1.0\n");
     printw("%s", "---------------------------\n\n");
 
     char* bad_chars = "!@#$%^&*()";  // Запрет символы
     char input[100];
 
-    printw("Введите строку: ");
-    scanw("%s", input);
+    while (true) {
+        printw("Введите строку: ");
+        scanw("%s", input);
+        input[strcspn(input, "\n")] = 0;
 
-    input[strcspn(input, "\n")] = 0;
-
-    if (bad_input(input, bad_chars)) {
-        printw("Введеная строка содержит запрещенные символы.\n");
-    } else {
-        printw("Введена строка: %s\n", input);
+        if (bad_input(input, bad_chars)) {
+            clear();
+            printw("Введеная строка содержит запрещенные символы\n\n");
+        } else {
+            printw("Введена строка: %s\n", input);
+            break;
+        }
     }
-
     printw("\nНажмите любую клавишу для выхода...\n");
     getch();
 }
@@ -45,4 +47,3 @@ int main() {
     output();
     return 0;
 }
-
